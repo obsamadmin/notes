5d50f1e82c4a3f5e45135404f325f54f2adedb0f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-07-14 18:21:26 +0100

    TASK-46279 Update Notes Activity URL generation due to Activity params modification (#111)

--- wiki-webapp/src/main/webapp/javascript/eXo/wiki/notesExtensions.js
+++ wiki-webapp/src/main/webapp/javascript/eXo/wiki/notesExtensions.js
@@ -16,7 +16,23 @@
           getTitle: activity => activity && activity.title || '',
           getSummary: (activity) => activity.templateParams
                                     && Vue.prototype.$utils.htmlToText(activity.templateParams.page_exceprt),
-          getSourceLink: (activity) => activity.templateParams.page_url,
+          getSourceLink: (activity) => {
+            try {
+              const templateParams = activity.templateParams || {};
+              if (templateParams.page_url) {
+                return activity.templateParams.page_url;
+              }
+              if (templateParams.page_type === 'group') {
+                return `${eXo.env.portal.context}/g/${templateParams.page_owner.replace(/\//g, ':')}/${templateParams.page_owner.replace('/spaces/', ':')}/wiki/${templateParams.page_id}`;
+              } else if(templateParams.page_type === 'portal') {
+                return `${eXo.env.portal.context}/${templateParams.page_owner}/wiki/${templateParams.page_id}`;
+              }
+            } catch (e) {
+              // eslint-disable-next-line no-console
+              console.warn('It seems that notes URL building is compromised. The activity will be displayed anyway with empty link', e);
+            }
+            return '#';
+          },
           canShare: () => true,
         },
       });
