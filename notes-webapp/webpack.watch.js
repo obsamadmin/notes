e05c2fdcb6935331db46fc0b4c5a71bdf515fd47
lbengayed <lbengayed@exoplatform.com>
2021-10-01 16:58:13 +0100

    Task-49179: Export_note_US_01 : Import notes

--- notes-webapp/webpack.watch.js
+++ notes-webapp/webpack.watch.js
@@ -5,7 +5,7 @@ const webpackCommonConfig = require('./webpack.prod.js');
 // the display name of the war
 const app = 'notes';
 
-const exoServerPath = "/home/exo/Downloads/plf-enterprise-tomcat-standalone-6.3.x-ux-20210915.032051-317/platform-6.3.x-ux-SNAPSHOT";
+const exoServerPath = "/exo-server";
 
 let config = merge(webpackCommonConfig, {
   output: {
