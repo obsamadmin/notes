993a23799130f8fab976c46e2e4debf993e924c7
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2021-10-14 15:48:50 +0100

    Task-49207:   Fix feedbacks related to export notes (#278)
    * Task-49207: Export_note_US_03 : Export notes fix retour

--- notes-webapp/src/main/webapp/vue-app/notes/components/NoteTreeviewDrawer.vue
+++ notes-webapp/src/main/webapp/vue-app/notes/components/NoteTreeviewDrawer.vue
@@ -99,22 +99,21 @@
             </v-list-item>
           </template>
           <template v-if="items && items.length && exportNotes">
-            <v-container fluid font-size="25">
-              <v-checkbox
-                v-model="checkbox"
-                :label="selectExportLabel"
-                class="checkbox" />
-            </v-container>
+            <v-checkbox
+              v-model="checkbox"
+              :label="selectExportLabel"
+              class="checkbox mt-0 pl-9" />
             <v-treeview
               v-if="reload"
               v-model="selectionNotes"
+              ref="treeSearch"
               :items="allItemsHome"
-              :active="active"
+              :open.sync="openLevel"
               class="treeview-item"
               item-key="noteId"
               hoverable
               selectable
-              open-all
+              open-on-click
               :selection-type="selectionType"
               transition />
           </template>
@@ -157,7 +156,7 @@
                 max-width="90"
                 contain
                 eager />
-              <p class="title mt-3 text-light-color">{{ $t('notes.label.noteSearchNotFound') + search }}</p>
+              <p class="mt-3 text-light-color">{{ $t('notes.label.noteSearchNotFound') + search }}</p>
             </div>
           </template>
         </v-col>
@@ -215,7 +214,7 @@ export default {
     spaceDisplayName: eXo.env.portal.spaceDisplayName,
     breadcrumb: [],
     destinationNote: {},
-    selectionType: 'independent',
+    selectionType: 'leaf',
     displayArrow: true,
     render: true,
     closeAll: true,
@@ -254,6 +253,9 @@ export default {
         return this.$t('notes.label.export.selectAll');
       }
     },
+    openLevel(){
+      return [this.home.noteId];
+    }
   },
   watch: {
     search() {
@@ -266,10 +268,10 @@ export default {
     checkbox() {
       if (this.checkbox){
         this.selectionNotes=[this.home.noteId];
-        this.selectionType='leaf';
+        this.$refs.treeSearch.updateAll(true);
       } else {
         this.selectionNotes= [];
-        this.selectionType='independent';
+        this.$refs.treeSearch.updateAll(false);
         this.open(this.home.noteId,'exportNotes');
       }
     },
