8c161ec3eb6850b9140de50fae2646cb88e956ce
Mohamed Amine Krout <mkrout@exoplatform.com>
2021-11-29 16:31:12 +0100

    TASK - 50490 :Add automatic delivery: disable the auto import by default

--- notes-service/src/main/java/org/exoplatform/wiki/service/NotesAutoImportService.java
+++ notes-service/src/main/java/org/exoplatform/wiki/service/NotesAutoImportService.java
@@ -110,7 +110,7 @@ public class NotesAutoImportService implements Startable {
 
   private String               frKnowledgeBaseSpaceDescription            = "Espace pour la base de connaissance eXo";
 
-  private boolean              importEnabled                              = true;
+  private boolean              importEnabled                              = false;
 
   private String               importConflictMode                         = "replaceAll";
 
